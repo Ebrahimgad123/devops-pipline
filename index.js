b1a87c8641aa3008d39cc523762029b934ad082a
Ibrahimgad <ibrahim.abo.khalil05@gmail.com>
2026-02-14 16:11:49 +0200

    add ci pipeline

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 console.log('Hello Devops!');
 console.log('i am learning  ci/cd pipeline using github actions');
+console.log('i am learning  ci/cd pipeline using github actions');
+console.log('i am learning  ci/cd pipeline using github actions');
+console.log('i am learning  ci/cd pipeline using github actions');
+console.log('i am learning  ci/cd pipeline using github actions');
